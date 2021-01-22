d821df493cb6ce815867d0f27821385056778d17
Piper <bates.sarah42@gmail.com>
2021-01-22 15:58:50 +0000

    minor changes

---
+++ domObjects.js
@@ -0,0 +1,18 @@
+//DOM Objects
+
+const pokeBox = document.querySelector("#poke-box");
+const pokemonName = document.querySelector("#poke-name");
+const pokemonHP = document.querySelector("#poke-hp");
+const pokemonInfoBox = document.querySelector("#misc-info-box");
+const pokemonImg = document.querySelector("#poke-image");
+const pokemonMoveBox = document.querySelector("#move-info");
+let pokemonMove1 = document.querySelector("#move1");
+let pokemonMove2 = document.querySelector("#move2");
+let pokemonAbility = document.querySelector("#ability");
+let button = document.querySelector("#randomGet");
+let button2 = document.querySelector("#dexGet");
+let typeBox = document.querySelector("#type-box");
+let input = document.querySelector("#input");
+let previous = document.querySelector("#left");
+let next = document.querySelector("#right");
+let hidden = document.querySelector("#hide-box");
